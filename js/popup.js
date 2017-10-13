2d8683749e558a0af9742bc94d8318b11d5516a5
Olga Duka <olgaduk@mail.ru>
2017-10-13 21:28:55 +0600

    Меню - исправление ошибок

--- js/popup.js
+++ js/popup.js
@@ -2,6 +2,9 @@ var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.page-header__toggle');
 var navTop = document.querySelector('.page-header__panel');
 
+navMain.classList.add('main-nav--closed');
+navTop.classList.add('page-header__panel--menu-closed');
+
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
