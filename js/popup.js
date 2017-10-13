b6fe13d6664553149255bb50dc28caf4f7671f46
Olga Duka <olgaduk@mail.ru>
2017-10-13 11:14:09 +0600

    Кнопка закрытия-открытия меню. Заработала

--- js/popup.js
+++ js/popup.js
@@ -5,15 +5,6 @@ var navTop = document.querySelector('.page-header__panel');
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    navTop.classList.remove('page-header__panel--bottom-btn-gum');
-    navTop.classList.add('page-header__panel--top-btn-cross');
-  } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
-    navTop.classList.remove('page-header__panel--top-btn-cross');
-    navTop.classList.add('page-header__panel--bottom-btn-gum');
-  }
+  navMain.classList.toggle('main-nav--closed');
+  navTop.classList.toggle('page-header__panel--menu-closed');
 });
